0b35467b66daa35faf0b86679aa4f1aac5dde591
Andrew Snead <andrewcsnead@gmail.com>
2014-10-19 00:46:27 +0100

    deleted dup prototype method

--- src/Rpsls.js
+++ src/Rpsls.js
@@ -31,14 +31,6 @@ function Draw() {
 }
 
 Rules.prototype.winningChoiceOf = function (choice1, choice2) {
-  return ["Draw", "Paper", "Rock", "Rock", "Spock",
-          "Paper", "Draw", "Scissors", "Lizard", "Paper",
-          "Rock", "Scissors", "Draw", "Scissors", "Spock",
-          "Rock", "Lizard", "Scissors", "Draw", "Lizard",
-          "Spock", "Paper", "Spock", "Lizard", "Draw"][choice1.index * 5 + choice2.index];
-}
-
-Rules.prototype.winningChoiceOf = function (choice1, choice2) {
   return [new Draw, choice2, choice1, choice1, choice2,
           choice1, new Draw, choice2, choice2, choice1,
           choice2, choice1, new Draw, choice1, choice2,
